9a398a9a27eb1a2d0c4c9c3e4777213f77dbffab
philipmogul <philipp1mogul@gmail.com>
2024-05-13 01:55:03 +0300

    Angular js coding

---
+++ angularjs.php
@@ -0,0 +1,24 @@
+<?php 
+
+// Angular Js 
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Angular Js</title>
+</head>
+<body>
+
+
+
+	<link href="style.css" rel="stylesheet" />
+	<link href="bootstrap/css/bootstrap.css" rel="stylesheet" />
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+	<script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
+	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="angular.min.js"></script>
+</body>
+</html>
